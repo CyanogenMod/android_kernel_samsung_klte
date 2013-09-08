bd279f73a4597a42b0de70af3645f23e9b85646d
Herbert Xu <herbert@gondor.apana.org.au>
2013-09-08 14:33:50 +1000

    crypto: api - Fix race condition in larval lookup
    commit 77dbd7a95e4a4f15264c333a9e9ab97ee27dc2aa upstream.
    
    crypto_larval_lookup should only return a larval if it created one.
    Any larval created by another entity must be processed through
    crypto_larval_wait before being returned.
    
    Otherwise this will lead to a larval being killed twice, which
    will most likely lead to a crash.
    
    Reported-by: Kees Cook <keescook@chromium.org>
    Tested-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	crypto/api.c

--- crypto/api.c
+++ crypto/api.c
@@ -42,6 +42,8 @@ static inline struct crypto_alg *crypto_alg_get(struct crypto_alg *alg)
 	return alg;
 }
 
+static struct crypto_alg *crypto_larval_wait(struct crypto_alg *alg);
+
 struct crypto_alg *crypto_mod_get(struct crypto_alg *alg)
 {
 	return try_module_get(alg->cra_module) ? crypto_alg_get(alg) : NULL;
@@ -153,11 +155,13 @@ static struct crypto_alg *crypto_larval_add(const char *name, u32 type,
 	up_write(&crypto_alg_sem);
 
 	//patch from kernel 3.13.7 (refer to https://www.kernel.org/ & https://lkml.org/lkml/2013/9/7/139)
+
 	if (alg != &larval->alg) {
 		kfree(larval);
 		if (crypto_is_larval(alg))
 			alg = crypto_larval_wait(alg);
 	}
+
 	return alg;
 }
 
