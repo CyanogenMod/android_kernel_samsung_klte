e1a60a512eb1b66fa03b1baf75dd488351318f19
Jorge Solano Altamirano <jsolano@codeaurora.org>
2013-11-06 17:14:09 -0800

    msm: vidc: Add support to request frame level QP information
    The resulting QP information for H264 is the slice level
    QP averaged over whole frame; for all other codecs its
    frame level QP.
    
    Change-Id: I288904b05d886c1e94fc113b8bd3fba5b13b48f3
    Signed-off-by: Jorge Solano Altamirano <jsolano@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -3134,6 +3134,9 @@ enum hal_extradata_id msm_comm_get_hal_extradata_index(
 	case V4L2_MPEG_VIDC_EXTRADATA_MPEG2_SEQDISP:
 		ret = HAL_EXTRADATA_MPEG2_SEQDISP;
 		break;
+	case V4L2_MPEG_VIDC_EXTRADATA_FRAME_QP:
+		ret = HAL_EXTRADATA_FRAME_QP;
+		break;
 	case V4L2_MPEG_VIDC_EXTRADATA_LTR:
 		ret = HAL_EXTRADATA_LTR_INFO;
 		break;
