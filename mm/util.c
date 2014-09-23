542390c6e0a2c1708eadb27aa3674e1819e0d133
Al Viro <viro@zeniv.linux.org.uk>
2014-09-23 10:17:29 -0600

    nick kvfree() from apparmor
    too many places open-code it
    
    Conflicts:
    	mm/util.c
    
    Bug: 17871993
    Change-Id: I007f4b663d7af564b2ce4009f5e13eeeeb82929a
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Git-commit: 39f1f78d53b9bcbca91967380c5f0f2305a5c55f
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [jgebben@codeaurora.org: Remove redundant apparmor code not present upstream]
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>
    
    Conflicts:
    	mm/util.c

--- mm/util.c
+++ mm/util.c
@@ -4,6 +4,10 @@
 #include <linux/export.h>
 #include <linux/err.h>
 #include <linux/sched.h>
+#include <linux/security.h>
+#include <linux/swap.h>
+#include <linux/swapops.h>
+#include <linux/vmalloc.h>
 #include <asm/uaccess.h>
 
 #include "internal.h"
@@ -341,7 +345,6 @@ int __attribute__((weak)) get_user_pages_fast(unsigned long start,
 }
 EXPORT_SYMBOL_GPL(get_user_pages_fast);
 
-
 /**
  * get_cmdline() - copy the cmdline value to a buffer.
  * @task:     the task whose cmdline value to copy.
@@ -390,6 +393,15 @@ out_mm:
 out:
 	return res;
 }
+
+void kvfree(const void *addr)
+{
+	if (is_vmalloc_addr(addr))
+		vfree(addr);
+	else
+		kfree(addr);
+}
+EXPORT_SYMBOL(kvfree);
 
 /* Tracepoints definitions. */
 EXPORT_TRACEPOINT_SYMBOL(kmalloc);
