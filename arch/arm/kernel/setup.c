99e36269f5047fcd44b5d1bf93221b03adfd7ec5
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-05-23 16:41:38 -0600

    arm/kernel/setup.c: Add MM bootloader compat shim
    * Stop the dying
    
    Change-Id: I072ac972219e704ab7732d8dc98f0d07ae304fb6

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -596,12 +596,25 @@ static int __init early_mem(char *p)
 }
 early_param("mem", early_mem);
 
+/*
+ * Pre-M bootloader passes this value
+ */
+static int __init msm_hw_rev_setup_legacy(char *p)
+{
+	system_rev = memparse(p, NULL);
+			return 0;
+}
+early_param("samsung.board_rev", msm_hw_rev_setup_legacy);
+
+/*
+ * M bootloader (and beyond?) passes this value
+ */
 static int __init msm_hw_rev_setup(char *p)
 {
 	system_rev = memparse(p, NULL);
 			return 0;
 }
-early_param("samsung.board_rev", msm_hw_rev_setup);
+early_param("androidboot.revision", msm_hw_rev_setup);
 
 static void __init
 setup_ramdisk(int doload, int prompt, int image_start, unsigned int rd_sz)
