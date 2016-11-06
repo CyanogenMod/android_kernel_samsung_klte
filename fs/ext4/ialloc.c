075b0db0685427fd2efcab04f1f3c3a337f68ff9
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-11-06 17:57:44 +0100

    ext4: fix ialloc.c

--- fs/ext4/ialloc.c
+++ fs/ext4/ialloc.c
@@ -735,11 +735,6 @@ got:
 	if (err)
 		goto fail;
 
-	BUFFER_TRACE(group_desc_bh, "get_write_access");
-	err = ext4_journal_get_write_access(handle, group_desc_bh);
-	if (err)
-		goto fail;
-
 	/* We may have to initialize the block bitmap if it isn't already */
 	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_GDT_CSUM) &&
 	    gdp->bg_flags & cpu_to_le16(EXT4_BG_BLOCK_UNINIT)) {
@@ -771,6 +766,11 @@ got:
 		if (err)
 			goto fail;
 	}
+
+    BUFFER_TRACE(group_desc_bh, "get_write_access");
+	err = ext4_journal_get_write_access(handle, group_desc_bh);
+	if (err)
+		goto fail;
 
 	/* Update the relevant bg descriptor fields */
 	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_GDT_CSUM)) {
